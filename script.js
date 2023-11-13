c7d4ec12a3b6b2e89f5e14f84a174b7035f662ef
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-11-13 19:45:09 +0530

    latest

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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";
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
